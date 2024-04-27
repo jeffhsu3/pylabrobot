cf65fe0d1648d2e9f34d25d3139466e86ab14fe8
jeffhsu3 <jeffhsu3@gmail.com>
2024-04-26 21:39:29 -0700

    initial tests

--- pylabrobot/resources/opentrons/load_tests.py
+++ pylabrobot/resources/opentrons/load_tests.py
@@ -1,1 +1,20 @@
 # TODO!
+import textwrap
+from typing import cast
+import unittest
+
+from pylabrobot.resources import Plate, TipRack, TubeRack
+from pylabrobot.resources.coordinate import Coordinate
+from pylabrobot.resources.opentrons import load_opentrons_resource
+
+
+class OpentronsLoadTests(unittest.TestCase):
+  """ Tests for the Opentrons load functions. """
+
+  def test_load_opentrons_resource(self):
+    name = 'PCR_plate'
+    hm = cast(Plate, load_opentrons_resource(
+      "opentrons_96_aluminumblock_nest_wellplate_100ul",
+      name=name,
+      version=1,
+      ))
